0ef88b9765599874594a7d6f8945a2c08143d704
Jordan Irwin <antumdeluge@gmail.com>
2021-07-22 19:21:31 -0700

    Add config & script for generating HTML docs with LDoc...
    https://stevedonovan.github.io/ldoc/

---
+++ docs/gendoc.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# Place this file in mod "docs" directory
+
+docs="$(dirname $(readlink -f $0))"
+root="$(dirname ${docs})"
+config="${docs}/config.ld"
+
+cd "${root}"
+
+# Clean old files
+rm -rf "${docs}/reference"
+
+# Create new files
+ldoc -c "${config}" -d "${docs}/reference" "${root}"
